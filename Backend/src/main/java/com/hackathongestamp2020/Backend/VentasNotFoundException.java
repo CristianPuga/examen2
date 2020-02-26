7dd1f0df3bc8ffe1c21736e2dada89ca2fe7873e
CristianPuga <cristian99puga@gmail.com>
2020-02-26 15:59:40 +0100

    pruebas

--- Backend/src/main/java/com/hackathongestamp2020/Backend/VentasNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.hackathongestamp2020.Backend;
-
-public class VentasNotFoundException extends RuntimeException {
-
-    VentasNotFoundException(String id) {
-        super("Could not find customer " + id);
-    }
-}
